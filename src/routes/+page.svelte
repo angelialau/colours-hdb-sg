b0aa6829e7b9321844f9ef2cd725f2c32548f0ba
Angelia Lau <angelialkm@gmail.com>
2024-04-10 09:43:42 -0400

    Feat: Add proj title and desc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>HDB ColourSG</h1>
+<p>Colors of Housing Development Board (HDB) Blocks in Singapore</p>
